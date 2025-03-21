4dd67017db459acb69f03a57a4c41c0b779a3444
tedtasman <tktasman@gmail.com>
2025-03-21 11:29:35 -0400

    add venv

--- backend/app.py
+++ backend/app.py
@@ -2,19 +2,18 @@ from urllib import request
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity, create_access_token
-from flask_sqlalchemy import SQLAlchemy
+import sqlite3
 
 app = Flask(__name__)
 CORS(app)
 app.config["JWT_SECRET_KEY"] = "secret_key"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///nittanybusiness.db'
 jwt = JWTManager(app)
-db = SQLAlchemy(app)
 
-class User(db.Model):
-    __tablename__ = 'Users'
-    email = db.Column(db.String(30), primary_key=True)
-    password = db.Column(db.String(30), nullable=False)
+
+def get_db_connection():
+    conn = sqlite3.connect('nittanybusiness.sqlite')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 @app.route('/api/hello', methods=['GET'])
 def hello():
@@ -33,6 +32,18 @@ def login():
     # receive request, get email and password
     data = request.get_json()
     email, password = data.get('email'), data.get('password')
+
+    # check if user exists
+    user = User.query.filter_by(email=email).first()
+
+    if not user or user.password != password:
+        return jsonify({"msg": "Bad email or password"}), 401
+
+    # create access token
+    access_token = create_access_token(identity=email)
+
+    return jsonify(access_token=access_token), 200
+
 
 
 
